2dc7c79a970578a47cb614109cadabeb77778777
Obscuriosity <tony.obscuriosity@gmail.com>
2020-09-20 19:01:01 +0100

    Update PID_motor_Rotation.py

--- PID_motor_Rotation.py
+++ PID_motor_Rotation.py
@@ -309,7 +309,6 @@ while True:
             RB = dataList[2]
             if LB == 0 or FB == 0 or RB == 0: # if bumpers are hit, Stop.
                 if Stopped == False:
-                    velocity = 0
                     Stopped = True
                     if LB == 0:
                         SpinLeft()
